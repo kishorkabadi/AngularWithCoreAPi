78a6e4b35968a0b7a9971ab959f8af331b3fc954
Kishor Kabadi <kishor.kabadikk@gmail.com>
2023-08-10 23:50:53 +0530

    Add http headers

--- src/app/apiservice.service.ts
+++ src/app/apiservice.service.ts
@@ -11,26 +11,35 @@ export class ApiserviceService {
 
   constructor(private http: HttpClient) { }
 
+  getHeaders() {
+
+    return new HttpHeaders({
+      'Content-Type': 'application/json', // Adjust the content type if needed
+      'Access-Control-Allow-Origin': '*', // Adjust the origin as needed
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // Adjust the allowed methods as needed
+    });
+  }
+
   // Customer
   getCustomerList(): Observable<CustomerModule[]> {
     return this.http.get<any[]>(this.apiUrl + 'Customers');
   }
   getCustomer(id: any): Observable<CustomerModule> {
-    return this.http.get<CustomerModule>(this.apiUrl + 'Customer/'+id);
+    return this.http.get<CustomerModule>(this.apiUrl + 'Customer/' + id);
   }
 
   addCustomer(emp: any): Observable<any> {
-    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    const httpOptions = { headers:  this.getHeaders() };
     return this.http.post<any>(this.apiUrl + 'Customer', emp, httpOptions);
   }
 
   updateCustomer(emp: any): Observable<any> {
-    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
-    return this.http.put<any>(this.apiUrl + 'Customer/'+emp.id, emp, httpOptions);
+    const httpOptions = { headers: this.getHeaders() };
+    return this.http.put<any>(this.apiUrl + 'Customer/' + emp.id, emp, httpOptions);
   }
 
   deleteCustomer(empId: number): Observable<number> {
-    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    const httpOptions = { headers: this.getHeaders()};
     return this.http.delete<number>(this.apiUrl + 'Customer/' + empId, httpOptions);
   }
 }
